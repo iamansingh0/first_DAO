17577e67ae19a2d31364d38ae28be36edbfa8ced
iamansingh0 <amankumar7355197337@gmail.com>
2022-07-07 21:50:49 +0530

    initial commit

---
+++ scripts/1-initialize-sdk.js
@@ -0,0 +1,19 @@
+import { ThirdwebSDK } from "@thirdweb-dev/sdk";
+import ethers from "ethers";
+import {} from 'dotenv/config';
+
+const provider = new ethers.providers.JsonRpcProvider(process.env.ALCHEMY_API_URL);
+const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+const sdk = new ThirdwebSDK(wallet);
+
+(async () => {
+  try {
+    const address = await sdk.getSigner().getAddress();
+    console.log("SDK initialized by address:", address)
+  } catch (err) {
+    console.error("Failed to get apps from the sdk", err);
+    process.exit(1);
+  }
+})();
+
+export default sdk;
